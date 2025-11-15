2b0f168e5212a68e4e5c6e444dda5930be7b21cd
Bikram Ghoshal <bikramghoshal6337@gmail.com>
2025-11-16 02:04:00 +0530

    first problem weired

--- Introductory Problems/Weiredalgorith.cpp
+++ Introductory Problems/Weiredalgorith.cpp
@@ -1,5 +1,24 @@
 #include<bits/stdc++.h>
-using namespace std ;
-int main(){
-    
+using namespace std;
+#define int long long
+int32_t main(){
+  int n;
+  cin>>n;
+  vector<int>arr;
+  arr.push_back(n);
+  int a = 0;
+  while(n!=1){
+    if(n%2!=0){
+      n = n*3+1;
+      arr.push_back(n);
+    }
+    else{
+      n=n/2;
+      arr.push_back(n);
+    }
+  }
+  for(int i = 0; i<arr.size(); i++){
+    cout<<arr[i]<<" ";
+  }
+ return 0;
 }
